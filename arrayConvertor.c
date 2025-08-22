651c6cf9de3b44dfef52c5f14fd6327392cd8974
redriot88 <166695662+redriot88@users.noreply.github.com>
2025-08-23 09:09:07 +1000

    Create arrayConvertor.c
    array convertor to nth power reading user input of array[5]

---
+++ arrayConvertor.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+
+#define ARRAY_SIZE 5 //defined constant for array ARRAY_SIZE
+
+int main(void){
+    int inputArray[ARRAY_SIZE];
+    int outputArray[ARRAY_SIZE];
+
+    printf("Enter 5 values separated by spaces ");
+    for (int i = 0; i < ARRAY_SIZE; i++){
+        scanf("%d", &inputArray[i]);
+    }
+    int n;
+
+    printf("Enter the value of n ");
+    scanf("%d", &n);
+
+    printf("Input array is: ");
+    for (int j = 0; j < ARRAY_SIZE; j++){
+        printf("%d ", inputArray[j]);
+    }
+    printf("\n");
+
+    printf("Output array is: ");
+    for (int j = 0; j < ARRAY_SIZE; j++){
+        outputArray[j] = pow(inputArray[j], n);
+        printf("%d ", outputArray[j]);
+    }
+    printf("\n");
+
+    return 0;
+}
