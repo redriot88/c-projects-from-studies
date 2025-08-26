55bbfcf3d871bdc67850ced4b084f57d11059fb4
redriot88 <166695662+redriot88@users.noreply.github.com>
2025-08-26 19:01:40 +1000

    Create savePlayerLetter.c
    added savePlayerLetter.c function whereby function write in a file

---
+++ savePlayerLetter.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+//define struct player_t as first char letter.
+typedef struct {
+    char letter;
+} player_t;
+
+
+//savePlayerName function
+int savePlayerName (player_t p, char filename []){
+    FILE* fp = fopen("player.txt", "w");
+    if (fp == NULL){
+        printf("error\n");
+        return 1; 
+    }
+    fprintf(fp, "%c\n", p.letter); //to write player's letter
+    fclose(fp);
+    return 0; //return 1 for success
+}
+
+int main(void){
+    player_t player; //declare player
+    
+    FILE* fp = NULL; //file stream
+    fp = fopen("player.txt", "w");
+    // FILE* fopen(const char* filename, const char* mode); 
+
+
+    if (fp == NULL){
+        printf("file error\n");
+        return 1;
+    } else {
+        scanf("%c", &player.letter);
+
+        savePlayerName(player, "player.txt");
+        
+        fclose(fp);
+        printf("Player name saved.\n");
+
+        return 0;
+    }
+}
